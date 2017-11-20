ae507441848195afd9eb34e22bf44b199f2ad539
Sumit Mukhija <sumit@jova.io>
2017-11-20 13:54:28 +0530

    Initial commit

---
+++ Github Lookup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Github Lookup
+//
+//  Created by Sumit on 11/20/17.
+//  Copyright © 2017 Sumit Mukhija. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
