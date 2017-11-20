6a84dbc699c681ecbc06f63786e13246f9d92f12
Sumit Mukhija <sumit@jova.io>
2017-11-20 16:07:57 +0530

    Now presenting the tab on search button action

--- Github Lookup/Helper classes/BaseViewController.swift
+++ Github Lookup/Helper classes/BaseViewController.swift
@@ -12,9 +12,48 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = COLORS.APP_BACKGROUND
+        hideNavigation()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+    }
+    
+    func setTabAttribs(){
+        self.tabBarController?.tabBar.isTranslucent = false
+        self.tabBarController?.tabBar.barTintColor = COLORS.APP_BACKGROUND
+        self.tabBarController?.tabBar.shadowImage = UIImage()
+        self.tabBarController?.tabBar.backgroundImage = UIImage()
+    }
+    
+    func setNavAttribs()
+    {
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.barTintColor = COLORS.APP_BLUE
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+    }
+    
+    func hideNavigation(){
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    func showNavigation(){
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    func setBackBtn()
+    {
+        let backImg = UIImage(named: "back")
+        let backButton = UIButton(type: .custom)
+        backButton.bounds = CGRect(x: 0, y: 0, width: 24.0, height: 24.0)
+        backButton.setImage(backImg, for: .normal)
+        backButton.addTarget(self, action: #selector(backBtnPressed(sender:)), for: .touchUpInside)
+        backButton.clipsToBounds = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+    }
+    
+    @objc func backBtnPressed(sender: AnyObject)
+    {
+        self.navigationController?.popViewController(animated: true)
     }
 }
--- Github Lookup/Helper classes/Constants.swift
+++ Github Lookup/Helper classes/Constants.swift
@@ -10,7 +10,7 @@ import UIKit
 
 struct COLORS
 {
-    static let APP_BACKGROUND = UIColor(red: 241/255.0, green: 241/255.0, blue: 241/255.0, alpha: 1.0)
+    static let APP_BACKGROUND = UIColor(red: 254/255.0, green: 254/255.0, blue: 254/255.0, alpha: 1.0)
     static let APP_BLUE = UIColor(red: 0/255.0, green: 201/255.0, blue: 255/255.0, alpha: 1.0)
     static let GIT_BLACK = UIColor(red: 37/255.0, green: 41/255.0, blue: 46/255.0, alpha: 1.0)
 }
