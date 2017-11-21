0d7606cdded22e8419c0038f149baf25ac5ceb1e
Sumit Mukhija <sumit@jova.io>
2017-11-21 09:51:42 +0530

    Added Core data manager

---
+++ Github Lookup/Helper classes/CoreDataHelper.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataHelper.swift
+//  Github Lookup
+//
+//  Created by Sumit on 11/21/17.
+//  Copyright © 2017 Sumit Mukhija. All rights reserved.
+//
+
+import Foundation
+
+class CoreDataHelper {
+
+}
